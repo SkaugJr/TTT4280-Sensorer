f47ed6b0b5815dd320e433f95912936001c10892
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-05 13:21:07 +0100

    k

--- Lab4/RadVelocity.m
+++ Lab4/RadVelocity.m
@@ -11,9 +11,9 @@ c=3*10^8;
 N_fft=2^(15);
 j=sqrt(-1);
 
-isRunning = true;
 
-while isRunning
+while true
+
 system(mypi,'lab4/./radar_sampler 31250 /home/Kollis/lab4/SAMPLE.bin','sudo');
 getFile(mypi,'/home/Kollis/lab4/SAMPLE.bin','C:\GitHub\TTT4280-Sensorer\Lab4');
 deleteFile(mypi, '/home/Kollis/lab4/SAMPLE.bin');
@@ -22,9 +22,9 @@ fid=fopen("SAMPLE.bin","rb");
 nomPeriod=fread(fid,1,"double");
 nomPeriod = nomPeriod * 1e-6;
 data=fread(fid,"uint16");
-nSamples=numel(data)/channels; %Antall sampler per ADC
+nSamples=numel(data)/channels; 
 fs=nSamples/nomPeriod;
-dataMatrix = reshape(data,channels,nSamples); %Matrise med verdiene til ADC-ene i hver rad
+dataMatrix = reshape(data,channels,nSamples);
 fclose(fid);
 
 I=dataMatrix(1,:)*VDD/(2.^nBits);
