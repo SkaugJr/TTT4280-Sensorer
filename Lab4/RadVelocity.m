967a0fa44b3d0c7c89091f010f2a96badd95c5ae
Kolbjørn Bølgen <skaugjr@gmail.com>
2024-03-05 12:59:50 +0100

    Forbedret kode

--- Lab4/RadVelocity.m
+++ Lab4/RadVelocity.m
@@ -2,35 +2,36 @@ clear mypi;
 clear;
 mypi=raspi("192.168.86.97","Kollis","646djohr");
 
-nSamples=31250;
+
 channels=2;
 nBits=12;
 VDD=3.3;
 f_radar=24.13*10^9;
 c=3*10^8;
-N_fft=2^(14);
+N_fft=2^(15);
 j=sqrt(-1);
-isRunning=true;
+
+isRunning = true;
 
 while isRunning
-system(mypi,'lab4/./radar_sampler nSamples /home/Kollis/lab4/SAMPLE.bin','sudo');
+system(mypi,'lab4/./radar_sampler 31250 /home/Kollis/lab4/SAMPLE.bin','sudo');
 getFile(mypi,'/home/Kollis/lab4/SAMPLE.bin','C:\GitHub\TTT4280-Sensorer\Lab4');
-deleteFile(mypi,'/home/Kollis/lab4/SAMPLE.bin');
+deleteFile(mypi, '/home/Kollis/lab4/SAMPLE.bin');
 
 fid=fopen("SAMPLE.bin","rb");
 nomPeriod=fread(fid,1,"double");
 nomPeriod = nomPeriod * 1e-6;
-fs=nSamples/nomPeriod;
 data=fread(fid,"uint16");
 nSamples=numel(data)/channels; %Antall sampler per ADC
+fs=nSamples/nomPeriod;
 dataMatrix = reshape(data,channels,nSamples); %Matrise med verdiene til ADC-ene i hver rad
 fclose(fid);
 
-I=dataMatrix(1,:)*VDD/(2^nBits);
-Q=dataMatrix(2,:)*VDD/(2^nBits);
+I=dataMatrix(1,:)*VDD/(2.^nBits);
+Q=dataMatrix(2,:)*VDD/(2.^nBits);
 
 x=I+j*Q;
-X=fft(X,N_fft);
+X=fft(x,N_fft);
 
 f = (0:nSamples-1) * fs / nSamples;
 [max_amp, max_idx] = max(abs(X));
@@ -39,9 +40,4 @@ f_d = f(max_idx);
 v_rad=c*f_d/(2*f_radar);
 
 disp(['Radiell hastighet: ', num2str(v_rad), ' m/s']);
-
-if kbhit
-        fprintf('Program avsluttet.\n');
-        isRunning = false;
-end
-end
+end 
